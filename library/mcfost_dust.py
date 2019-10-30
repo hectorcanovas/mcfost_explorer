75a73c5fb8a18089ec34aca8cbe95ff869052952
Hector Canovas <hcanovas@gaiam03.iuser.lan>
2019-10-30 09:38:30 +0100

    adding extra features to mcfost_polimg library

--- library/mcfost_dust.py
+++ library/mcfost_dust.py
@@ -11,7 +11,7 @@ class Dust():
 
     def read_lambda(self):
         """
-        Read wavelengths used for dust/SED/Images computations
+        Read wavelengths used for dust/SED/Images computations.
         """        
         hdu = fits.open(self.path + 'lambda.fits.gz')
         self.lambdas = hdu[0].data
@@ -19,7 +19,7 @@ class Dust():
 
     def read_pol(self):
         """
-        Read polarisability curve
+        Read polarisability curve.
         """        
         hdu = fits.open(self.path + 'polarizability.fits.gz')
         self.polar = hdu[0].data
@@ -27,21 +27,16 @@ class Dust():
 
     def read_phase_f(self):
         """
-        Read phase function
+        Read phase function.
         """        
         hdu        = fits.open(self.path + 'phase_function.fits.gz')
         self.phase = hdu[0].data
 
         
-    def get_mcwave(self, inp_wave = 2.2):
-        """
-        Finds closes wavelengt to input [micrometers]
-        """        
-        els          = [np.abs(inp_lambda - inp_wave) for inp_lambda in self.lambdas]
-        index        = np.where(els == np.min(els))[0][0]
-        self.ilambda = self.lambdas[index]
-
-
+    def read_all(self):
+        self.read_lambda()
+        self.read_phase_f()
+        self.read_pol()
 
 
 # Associated plotters ============================================================
@@ -52,33 +47,33 @@ class Dust():
         plt.yticks(fontsize = fontsize)
 
 
-    def plot_phase(self, fontsize = 16, figsize=[20,7], plt_show = True):
+    def plot_phase(self, fontsize = 16, figsize=[10,7], plt_show = True):
         if plt_show:
             fig = plt.figure(figsize=figsize)
         
-        self.make_canvas(xlabel = 'Log10(Phase Function)', ylabel = 'Scattering Angle', fontsize = fontsize)
+        self.make_canvas(xlabel = 'Scattering Angle', ylabel = r'$Log_{10}$(Phase Function)', fontsize = fontsize)
         plt.plot(self.angles, np.log10(self.phase))
         if plt_show:
             plt.show()    
 
             
-    def plot_polaris(self, fontsize = 16, figsize=[20,7], plt_show = True):
+    def plot_polaris(self, fontsize = 16, figsize=[10,7], plt_show = True):
         if plt_show:
             fig = plt.figure(figsize=figsize)
         plt.plot(self.angles, self.polar)
-        self.make_canvas(xlabel = 'Polarizability', ylabel = 'Scattering Angle', fontsize = fontsize)
+        self.make_canvas(xlabel = 'Scattering Angle', ylabel = 'Polarizability', fontsize = fontsize)
         plt.hlines(xmin=0,    xmax=180, y=0, linestyles=':')
         plt.vlines(ymin=-0.1, ymax=1.0, x=90, linestyles=':')
         if plt_show:
             plt.show()
 
     
-    def plot_scat(self, fontsize = 16, figsize=[20,7], fig_name = None):
+    def plot_scat(self, fontsize = 20, figsize=[30,10], fig_name = None):
         fig = plt.figure(figsize=figsize)
         plt.subplot(121)        
-        self.plot_phase(plt_show = False)
+        self.plot_phase(plt_show = False, fontsize = fontsize)
         plt.subplot(122)        
-        self.plot_polaris(plt_show = False)
+        self.plot_polaris(plt_show = False, fontsize = fontsize)
         plt.show()
         if fig_name:
             text = f'Figure saved as {fig_name}'
--- library/mcfost_polimg.py
+++ library/mcfost_polimg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.colorbar import colorbar
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+import matplotlib.ticker as ticker
 from astropy.io          import fits
 from astropy.convolution import convolve
 from astropy.convolution import Gaussian2DKernel
@@ -9,6 +12,7 @@ class ImgPol():
     def __init__(self, path = '', wave = '0.5'):
         self.path   = path
         self.wave   = wave
+        self.labels = ['I', 'Q', 'U', 'Idisc']        
 
     def read_fitscube(self):
         """
@@ -33,13 +37,44 @@ class ImgPol():
     def get_imgs(self):
         """
         Read & label Stokes-Parameter images
-        """        
-        self.I        = self.imgs[0]
-        self.Q        = self.imgs[1]
-        self.U        = self.imgs[2]
+        """
+        for i in range(3):
+            self.__setattr__(self.labels[i], self.imgs[i])
+        
+        # Disc in scattered light ===
         self.mask     = np.zeros([self.xdim, self.ydim]) + 1
         self.mask[self.yc, self.xc] = 0
-        self.Idisc    = self.I * self.mask # Disc in scattered light
+        self.__setattr__(self.labels[3], self.imgs[0] * self.mask)
+
+
+    def make_PSF(self, gauss_kernel = 5, input_psf = None):
+        """
+        Create PSF
+        Note: a real PSF is more complex than a Gaussian.
+        """        
+        if input_psf:
+            print('Loading user-given PSF')
+        else:
+            self.psf = Gaussian2DKernel(x_stddev=gauss_kernel, x_size = self.xdim, y_size = self.ydim)
+
+
+    def convolve(self):
+        """
+        Convolve images with PSF to emulate real observations
+        """        
+        for label in self.labels:
+            self.__setattr__(label, convolve(self.__getattribute__(label), self.psf))
+        self.make_pol()
+
+
+    def add_noise(self, SN = 5, noise_std = 1.0):
+        """
+        Add randomn noise to mimic real observations
+        """        
+        noise    = np.random.normal(1.0,noise_std,(self.xdim, self.ydim))               # Noise Array
+        noise    = noise/np.max(noise) * np.max(np.sqrt(self.Q**2 + self.U**2)) * 1/SN  # Scale it to P 
+        for inp in self.labels:
+            self.__setattr__(inp, self.__getattribute__(inp) + noise)
 
 
     def make_pol(self):
@@ -49,38 +84,37 @@ class ImgPol():
         self.P  = np.sqrt(self.Q**2 + self.U**2)
         np.seterr(divide='ignore', invalid='ignore') # To avoid Np complaining about division by zero
         self.dP = np.nan_to_num(self.P/self.Idisc, 0)
+        self.labels_pol = ['P', 'dP']    # Here I store derived (not directly observed) quantities
 
-
-    def define_PSF(self, kernel = 5, user_defined = None):
-        """
-        Create PSF
-        Note: a real PSF is more complex than a Gaussian.
-        """        
-        if user_defined:
-            print('Loading user-given PSF')
-        else:
-            self.psf = Gaussian2DKernel(x_stddev=kernel, x_size = self.xdim, y_size = self.ydim)
-
-
-    def convolve(self, kernel = 2):
-        """
-        Convolve images with PSF to emulate real observations
-        """        
-        self.I     = convolve(self.I,     self.psf)
-        self.Q     = convolve(self.Q,     self.psf)
-        self.U     = convolve(self.U,     self.psf)
-        self.Idisc = convolve(self.Idisc, self.psf)
-        self.make_pol()
 
         
-    def plot_pol_imgs(self, figsize = [30,7], cmap = 'viridis'):
+    def plot_pol_imgs(self, figsize = [30,7], fontsize = 18, bbox={'facecolor':'white', 'alpha':1.0, 'pad':7}):
         """
         Separate & Compute Polarised images
         """        
-        fig = plt.figure(figsize = figsize)
-        i = 0
+        fig    = plt.figure(figsize = figsize)
+        cmaps  = ['viridis', 'RdYlGn'  , 'RdYlGn'  , 'viridis']
+        tlabel = [r'$I_{disc}$', 'Stokes Q', 'Stokes U', r'$P_I$']
+        i      = 0
         for inp_img in [self.Idisc, self.Q, self.U, self.P]:
+            ax  = plt.subplot(141 + i)
+            img = plt.imshow(inp_img, cmap = cmaps[i], origin = 'lower')
+            plt.subplots_adjust(wspace = 0.0, hspace = 0)
+            plt.axis('off')
+            ax.text(self.xdim * 0.1, self.ydim*0.9, tlabel[i], fontsize = fontsize, bbox=bbox) # label
+            self.make_colorbar(ax, img, fontsize)
             i = i + 1
-            plt.subplot(140 + i)
-            plt.imshow(inp_img, cmap = cmap, origin = 'lower')
         plt.show()
+
+
+    def make_colorbar(self, axis_obj, img, fontsize = 18):
+                    #   Set Color Bar ================
+            ax_divider = make_axes_locatable(axis_obj)
+            cax        = ax_divider.append_axes("top", size="7%", pad="4.5%", aspect=0.075)
+            cb         = plt.colorbar(img, cax=cax, orientation="horizontal")
+            cb.ax.xaxis.set_label_position('top')
+            cb.locator = ticker.MaxNLocator(nbins=5) #To controll the amount of ticks in Color Table
+            cb.update_ticks()
+
+            cb.ax.tick_params(labelsize = fontsize*0.8, top = True, bottom = False, labeltop = True, labelbottom = False)
+            cb.ax.set_xlabel(self.head['BUNIT'], fontsize = fontsize*0.8)
